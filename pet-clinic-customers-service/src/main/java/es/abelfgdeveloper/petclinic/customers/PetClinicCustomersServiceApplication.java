f8f00812015404e1f3fb6251a30ef2add5f917ed
Abel <abeldevelop@users.noreply.github.com>
2020-11-16 21:42:37 +0100

    Create the structure

---
+++ pet-clinic-customers-service/src/main/java/es/abelfgdeveloper/petclinic/customers/PetClinicCustomersServiceApplication.java
@@ -0,0 +1,12 @@
+package es.abelfgdeveloper.petclinic.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicCustomersServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PetClinicCustomersServiceApplication.class, args);
+  }
+}
---
+++ pet-clinic-customers-service/src/test/java/es/abelfgdeveloper/petclinic/customers/PetClinicCustomersServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package es.abelfgdeveloper.petclinic.customers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicCustomersServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
